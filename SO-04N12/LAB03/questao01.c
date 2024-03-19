a30e4966c7ead10f76c6788bab064fe918143a55
33jonatassilva <gstylus840@gmail.com>
2024-03-19 03:25:32 +0000

    Adicionando exercícios

--- SO-04N12/LAB03/questao01.c
+++ SO-04N12/LAB03/questao01.c
@@ -1,20 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-int main (int argc, char *argv[]) {
-pid_t childpid = 0;
-int i, n;
-if (argc != 2){ /* check for valid number of command-line arguments */
-fprintf(stderr, "Usage: %s processes\n", argv[0]);
-return 1;
-}
-n = atoi(argv[1]);
-for (i = 1; i < n; i++){
-	if (childpid = fork()){
-		break;
-	}
-}
-fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
-i, (long)getpid(), (long)getppid(), (long)childpid);
+int main (void) {
+printf("I am process %ld\n", (long)getpid());
+printf("My parent is %ld\n", (long)getppid());
 return 0;
 }
